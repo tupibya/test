36adb0ab9ca43cab4f39720f7ff3cafc4faa15f7
Yukidaruma14 <tupibya@kywa.uk>
2026-07-29 12:45:44 +0900

    Add PWA support: manifest.json, service-worker.js, index.html, and svg icons

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'site-cache-v1';
+const PRECACHE_URLS = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icons/icon-192.svg',
+  '/icons/icon-512.svg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(PRECACHE_URLS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request))
+  );
+});
