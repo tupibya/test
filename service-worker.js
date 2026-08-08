9222668a6d75caac71f034fcad19a3c811e0c37a
Yukidaruma14 <tupibya@kywa.uk>
2026-08-08 17:16:16 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,13 @@
-const CACHE_NAME = 'site-cache-v1';
-const PRECACHE_URLS = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icons/icon-192.svg',
-  './icons/icon-512.svg'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(PRECACHE_URLS))
-  );
+self.addEventListener('install', (event) => {
+  console.log('Service Worker: Installed');
   self.skipWaiting();
 });
 
-self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
+self.addEventListener('activate', (event) => {
+  console.log('Service Worker: Activated');
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(resp => resp || fetch(event.request))
-  );
+self.addEventListener('fetch', (event) => {
+  // とりあえずネットワーク優先
+  event.respondWith(fetch(event.request));
 });
